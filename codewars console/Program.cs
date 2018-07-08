d55b9e4ebc71f36ca59499856eb1fde663354ccb
Иван <@IVANPC>
2018-07-08 20:06:51 +0300

    Добавьте файлы проекта.

---
+++ codewars console/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codewars_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Accum("CodeWars");
+
+
+        
+        }
+        public static String Accum(string s)
+        {
+            Console.WriteLine(string.Join("-", s.Select((x,i) => char.ToUpper(x) + new string (char.ToLower(x),i)))); // select(x,i) - х это значение, i Это индекс значения. В первой части создаются все  большие буквы Select((x,i) => char.ToUpper(x) затем добавляется строка с двумя параметрами x и i где х это значение а i это длина строки
+
+
+            Console.ReadKey();
+            return string.Join("-", s.Select((x,i)=> char.ToUpper(x)));
+        }
+
+
+    }
+      
+
+    }
+//s.Select(x=>x.IsUpper? x.ToUpper:x.ToLower).ToArray;
+
+//int i = Convert.ToInt32(year.ToString().Substring(year.ToString().Length - 2)) == 00 ? (year) : year + 100;
+ /*string a = "";
+char ch;
+            for (int i = 0; s.Length > i; i++)
+            {
+                ch = s[i];
+                string s3 = new String(ch, i + 1);
+s3 = s3.ToLower();
+                s3 = char.ToUpper(s3[0]) + s3.Substring(1);
+                a += s3 + "-";
+            }
+            a = a.Trim(new char[] { '-' });
+            Console.WriteLine(a);
+            Console.ReadKey();
+            return a;*/
---
+++ codewars console/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("codewars console")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("codewars console")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("bab2646a-f688-488e-b31f-ee7e6fc885e7")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
