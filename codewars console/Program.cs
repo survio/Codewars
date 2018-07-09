c27749f65fb664e29f348177fad19c6cb98409fa
Иван <@IVANPC>
2018-07-09 19:47:34 +0300

    123

--- codewars console/Program.cs
+++ codewars console/Program.cs
@@ -12,39 +12,54 @@ namespace codewars_console
         static void Main(string[] args)
         {
 
-            Accum("CodeWars");
+            Decode(".... . -.--   .--- ..- -.. .");
 
 
-        
+
         }
+        public static string Decode(string morseCode)
+        {
+            Console.WriteLine(string.Join(" ", morseCode.Split().Select(x => x)));
+            Console.ReadKey();
+            return morseCode.Split().Select(x => x).ToString();
+        }
+
         public static String Accum(string s)
         {
-            Console.WriteLine(string.Join("-", s.Select((x,i) => char.ToUpper(x) + new string (char.ToLower(x),i)))); // select(x,i) - х это значение, i Это индекс значения. В первой части создаются все  большие буквы Select((x,i) => char.ToUpper(x) затем добавляется строка с двумя параметрами x и i где х это значение а i это длина строки
+            Console.WriteLine(string.Join("-", s.Select((x, i) => char.ToUpper(x) + new string(char.ToLower(x), i)))); // select(x,i) - х это значение, i Это индекс значения. В первой части создаются все  большие буквы Select((x,i) => char.ToUpper(x) затем добавляется строка с двумя параметрами x и i где х это значение а i это длина строки
 
 
             Console.ReadKey();
-            return string.Join("-", s.Select((x,i)=> char.ToUpper(x)));
+            return string.Join("-", s.Select((x, i) => char.ToUpper(x)));
         }
 
 
     }
-      
-
-    }
+}
 //s.Select(x=>x.IsUpper? x.ToUpper:x.ToLower).ToArray;
 
 //int i = Convert.ToInt32(year.ToString().Substring(year.ToString().Length - 2)) == 00 ? (year) : year + 100;
- /*string a = "";
+/*string a = "";
 char ch;
-            for (int i = 0; s.Length > i; i++)
-            {
-                ch = s[i];
-                string s3 = new String(ch, i + 1);
+           for (int i = 0; s.Length > i; i++)
+           {
+               ch = s[i];
+               string s3 = new String(ch, i + 1);
 s3 = s3.ToLower();
-                s3 = char.ToUpper(s3[0]) + s3.Substring(1);
-                a += s3 + "-";
-            }
-            a = a.Trim(new char[] { '-' });
-            Console.WriteLine(a);
-            Console.ReadKey();
-            return a;*/
+               s3 = char.ToUpper(s3[0]) + s3.Substring(1);
+               a += s3 + "-";
+           }
+           a = a.Trim(new char[] { '-' });
+           Console.WriteLine(a);
+           Console.ReadKey();
+           return a;*/
+
+
+
+/*public static String Accum(string s)
+{
+    Console.WriteLine(string.Join("-", s.Select((x, i) => char.ToUpper(x) + new string(char.ToLower(x), i)))); // select(x,i) - х это значение, i Это индекс значения. В первой части создаются все  большие буквы Select((x,i) => char.ToUpper(x) затем добавляется строка с двумя параметрами x и i где х это значение а i это длина строки
+
+
+    Console.ReadKey();
+    return string.Join("-", s.Select((x, i) => char.ToUpper(x)));*/
